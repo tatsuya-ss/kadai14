45f41d24396f1acb41cd8c67e293922761de300e
tatsuya-ss <72326299+tatsuya-ss@users.noreply.github.com>
2021-01-09 17:11:39 +0900

    cellForRowAtにセルの初期化コードを追加

--- kadai13/CustomTableViewCell.swift
+++ kadai13/CustomTableViewCell.swift
@@ -8,14 +8,19 @@
 import UIKit
 
 final class CustomTableViewCell: UITableViewCell {
-    @IBOutlet private var checkImageView: UIImageView!
+    @IBOutlet private(set) var checkImageView: UIImageView!
     @IBOutlet private var contentLabel: UILabel!
     
-    //
     func configure(isChecked: Bool, name: String) {
         if isChecked == true {
             checkImageView.image = UIImage(named: CustomCellId.checkImageName)
         } 
         contentLabel.text = name
     }
+//これでもできるが、パフォーマンスの観点からcellForRowAtの中で初期化を行う事が推奨されている。
+//    override func prepareForReuse() {
+//        super.prepareForReuse()
+//        //画像の初期化
+//        checkImageView.image = nil
+//    }
 }
