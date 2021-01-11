62410337d2dc0df767074be09593f319a6c37480
tatsuya-ss <72326299+tatsuya-ss@users.noreply.github.com>
2021-01-11 12:07:21 +0900

    swiftlint導入後修正

--- kadai13/CustomTableViewCell.swift
+++ kadai13/CustomTableViewCell.swift
@@ -13,30 +13,14 @@ final class CustomTableViewCell: UITableViewCell {
 
     /// この画像を外で使うことはないのでprivateで閉じ込める
     private static let checkImage = UIImage(named: "check")
-    
+
     // セルが増えてくるとCustomCellIdの項目が増えて破綻しそうなのでセル側で定義
     static let reuseIdentifier = "Cell1"
-    
+
     func configure(isChecked: Bool, name: String) {
-<<<<<<< HEAD
-        if isChecked == true {
-            checkImageView.image = UIImage(named: ImageViewName.Name)
-        } else {
-            checkImageView.image = nil
-        }
-        contentLabel.text = name
-    }
-//これでもできるが、パフォーマンスの観点からcellForRowAtの中で初期化を行う事が推奨されているらしい
-//    override func prepareForReuse() {
-//        super.prepareForReuse()
-//        //画像の初期化
-//        checkImageView.image = nil
-//    }
-=======
         // 再利用に備えるため、必ず何かを代入する
         checkImageView.image = isChecked ? Self.checkImage : nil
-        
+
         contentLabel.text = name
     }
->>>>>>> da743359e8254cf445395aadac1c065d6c4634e2
 }
--- kadai13/TableViewController.swift
+++ kadai13/TableViewController.swift
@@ -8,34 +8,35 @@
 import UIKit
 
 final class TableViewController: UITableViewController {
-    
-    private let fruits = ["りんご","みかん","ばなな","パイナップル","りんご","みかん","ばなな","パイナップル","りんご","みかん","ばなな","パイナップル","りんご","みかん","ばなな","パイナップル","りんご","みかん","ばなな","パイナップル","りんご","みかん","ばなな","パイナップル","りんご","みかん","ばなな","パイナップル","りんご","みかん","ばなな","パイナップル","りんご","みかん","ばなな","パイナップル","りんご","みかん","ばなな","パイナップル","りんご","みかん","ばなな","パイナップル","りんご","みかん","ばなな","パイナップル","りんご","みかん","ばなな","パイナップル","りんご","みかん","ばなな","パイナップル","りんご","みかん","ばなな","パイナップル","りんご","みかん","ばなな","パイナップル","りんご","みかん","ばなな","パイナップル","りんご","みかん","ばなな","パイナップル","りんご","みかん","ばなな","パイナップル","りんご","みかん","ばなな","パイナップル","りんご","みかん","ばなな","パイナップル","りんご","みかん","ばなな","パイナップル","りんご","みかん","ばなな","パイナップル","りんご","みかん","ばなな","パイナップル","りんご","みかん","ばなな","パイナップル","りんご","みかん","ばなな","パイナップル"]
+
+    private let fruits = ["りんご", "みかん", "ばなな", "パイナップル"]
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //identifierの設定
+        //　identifierの設定
         tableView.register(
             UINib(nibName: "CustomTableViewCell", bundle: nil),
-            
+
             // CustomTableViewCellのIdentifierであることが明確となる
             forCellReuseIdentifier: CustomTableViewCell.reuseIdentifier
         )
     }
-    
+
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-    
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return fruits.count
     }
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        //xibで作成したセルを使用。
+        //　xibで作成したセルを使用。
         let cell = tableView.dequeueReusableCell(withIdentifier: CustomTableViewCell.reuseIdentifier, for: indexPath) as! CustomTableViewCell
-        
+
         cell.configure(isChecked: indexPath.row % 2 == 1, name: fruits[indexPath.row])
-        
+
         return cell
     }
 }
